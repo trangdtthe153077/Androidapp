0845d6e81552b91241af20788dff8cc1a638944f
namvdhe151370 <97592724+namvdhe151370@users.noreply.github.com>
2023-03-23 09:33:58 +0700

    Revert "fix connect db"
    This reverts commit ff1797248955f65e9762a2f6067da28c59888acd.

--- app/src/main/java/com/example/firebase/LogoutActivity.java
+++ app/src/main/java/com/example/firebase/LogoutActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -34,7 +33,6 @@ public class LogoutActivity extends AppCompatActivity {
     }
 
     private void logout() {
-        Toast.makeText(getApplicationContext(), "Logout Successful.", Toast.LENGTH_SHORT).show();
         Intent i = new Intent(LogoutActivity.this, MainActivity.class);
         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(i);
